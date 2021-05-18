35ef2102aa5c4e2b75b049b047802094bdea2df8
aquino-larissa <lariaquino7@gmail.com>
2021-05-17 23:40:05 -0300

    refatoração do código

--- toDo/priority/views.py
+++ toDo/priority/views.py
@@ -1,7 +1,6 @@
-from django.http import HttpResponse
-
+from django.shortcuts import render
 
 # Create your views here.
 
 def home_page(request):
-	return HttpResponse('<html><title>Priority to-do lists</title></html>')
+	return render(request, 'home.html')
